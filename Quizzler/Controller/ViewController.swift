fe3ad3c6629829044998df67c89a87be324234f3
alexmortelsison <alexmortelsison@gmail.com>
2024-04-03 08:43:41 +0900

    applied MVC

---
+++ Quizzler/Controller/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Alex Sison on 4/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var progressBar: UIProgressView!
+    @IBOutlet var falseButton: UIButton!
+    @IBOutlet var trueButton: UIButton!
+
+    var quizBrain = QuizBrain()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+
+    @IBAction func answerPresssedButton(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let userGotItRight = quizBrain.checkAnswer(userAnswer!)
+
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+
+        quizBrain.nextQuestion()
+
+        Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+
+    @objc func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        progressBar.progress = quizBrain.getProgress()
+    }
+}
---
+++ Quizzler/Model/QuizBrain.swift
@@ -0,0 +1,44 @@
+//
+//  QuizBrain.swift
+//  Quizzler
+//
+//  Created by Alex Sison on 4/3/24.
+//
+
+import Foundation
+
+struct QuizBrain {
+    let quiz = [Question(q: "Albert Einstein had trouble with mathematics when he was in school.", a: "False"),
+                Question(q: "The vapor produced by e-cigarettes is actually water.", a: "False"),
+                Question(q: "Anatomy considers the forms of macroscopic structures such as organs and organ systems.", a: "True"),
+                Question(q: "Instant mashed potatoes were invented by Canadian Edward Asselbergs in 1962.", a: "True"),
+                Question(q: "Tupac Shakur died due to complications from being stabbed in 1996.", a: "False"),
+                Question(q: "In \"Star Trek\", Klingons are aliens.", a: "True")]
+
+    var questionNumber = 0
+
+    func checkAnswer(_ userAnswer: String) -> Bool {
+        if userAnswer == quiz[questionNumber].answer {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func getQuestionText() -> String {
+        return quiz[questionNumber].text
+    }
+    
+    func getProgress() -> Float {
+        let progress = Float(questionNumber) / Float(quiz.count)
+        return progress
+    }
+    
+    mutating func nextQuestion() {
+        if questionNumber < quiz.count - 1 {
+            return questionNumber += 1
+        } else {
+            return questionNumber = 0
+        }
+    }
+}
